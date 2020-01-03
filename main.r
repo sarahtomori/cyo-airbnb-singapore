be0db59a257302635e4125ed2e70935e6076ce4f
sarahtomori <sarahtomorioyebo@Sarahs-MacBook-Pro.local>
2020-01-03 15:40:38 +0100

    Graphed listings vs prices

--- main.r
+++ main.r
@@ -4,37 +4,35 @@ library("magrittr", lib.loc="/Library/Frameworks/R.framework/Versions/3.6/Resour
 library("dplyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.6/Resources/library")
 data <- read_csv("./data.csv")
 
-# Create validation set as 10% of the dataset
+# Create test set as 10% of the dataset
 set.seed(1, sample.kind="Rounding")
 test_index <- createDataPartition(y = data$price, times = 1, p = 0.1, list = FALSE)
 airbnb_singapore <- data[-test_index,]
 temp <- data[test_index,]
 
-# Make sure that id and host_id in the validation set are present in the airbnb_singapore set
-validation <- temp %>% 
-  semi_join(airbnb_singapore, by = "id") %>%
+# Make sure that host_id in the test set are present in the airbnb_singapore set
+test_set <- temp %>%
   semi_join(airbnb_singapore, by = "host_id")
-# Add rows removed from validation set back into airbnb_singapore set
-removed <- anti_join(temp, validation)
+# Add rows removed from test set back into airbnb_singapore set
+removed <- anti_join(temp, test_set)
 airbnb_singapore <- rbind(airbnb_singapore, removed)
 rm(temp, data, removed)
 
-# Split airbnb_singapore data further in to test and training sets
-# Test set will be 10% of current airbnb_singapore data
+# Split airbnb_singapore data further in to validation and training sets
+# Validation set will be 10% of current airbnb_singapore data
 set.seed(1, sample.kind="Rounding")
 # if using R 3.5 or earlier, use `set.seed(1)` instead
 test_index2 <- createDataPartition(y = airbnb_singapore$price, times = 1, p = 0.1, list = FALSE)
 train_set <- airbnb_singapore[-test_index2,]
 temp2 <- airbnb_singapore[test_index2,]
 
-# Make sure userId and movieId in test set are also in training set
-test_set <- temp2 %>% 
-  semi_join(train_set, by = "id") %>%
+# Make sure host_id in validation are also in training set
+validation <- temp2 %>% 
   semi_join(train_set, by = "host_id")
-# Add rows removed from test set back into training set
-removed <- anti_join(temp2, test_set)
+# Add rows removed from validation back into training set
+removed <- anti_join(temp2, validation)
 train_set <- rbind(train_set, removed)
-rm(test_index2, temp2, removed)
+rm(temp2, removed)
 
 # Exploring the data
 
@@ -64,3 +62,7 @@ mean(airbnb_singapore$minimum_nights)
 mean(airbnb_singapore$price)
 
 # Visualisation of the data 
+# Price distributions in the training set
+airbnb_singapore %>% count(price) %>%
+ggplot(aes(n)) + geom_histogram(fill = "deepskyblue4", color = "azure4", bins = 30) +
+scale_x_log10() + ggtitle("Prices of vs. number of listings") + xlab("Prices") + ylab("Listings")
