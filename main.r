0cfcb9db4f194b3eef57f359c33337f53bfa7b1a
sarahtomori <sarahtomori.oyebo@gmail.com>
2020-01-08 23:14:32 +0100

    Lm models

--- main.r
+++ main.r
@@ -1,8 +1,32 @@
-library("readr", lib.loc="/Library/Frameworks/R.framework/Versions/3.6/Resources/library")
-library("caret", lib.loc="/Library/Frameworks/R.framework/Versions/3.6/Resources/library")
-library("magrittr", lib.loc="/Library/Frameworks/R.framework/Versions/3.6/Resources/library")
-library("dplyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.6/Resources/library")
-data <- read_csv("./data.csv")
+#Load the libraries
+
+library("readr")
+library("caret")
+library("magrittr")
+library("dplyr")
+library("wordcloud")
+library("SnowballC")
+library("corrplot")
+library("wesanderson")
+library("maps")
+library("mapproj")
+library("ggmap")
+library("textmining")
+library("Hmisc")
+library("rpart")
+library("rsq")
+library("glmnet")
+library("neuralnet")
+
+#Read the file with the Singapore Airbnb data. 
+
+#Singapore data set loaded from:
+#http://data.insideairbnb.com/singapore/sg/singapore/2019-11-26/visualisations/listings.csv
+
+dl <- tempfile()
+download.file("http://data.insideairbnb.com/singapore/sg/singapore/2019-11-26/visualisations/listings.csv", dl)
+
+data <- read_csv(dl)
 
 # Create test set as 10% of the dataset
 set.seed(1, sample.kind="Rounding")
@@ -48,10 +72,10 @@ summary(airbnb_singapore)
 # Check whether there are any missing values
 anyNA(airbnb_singapore)
 
-# Show unique values of movie ratings
+# Show unique values of prices
 unique(airbnb_singapore$price)
 
-# Show number of users, movies and genres
+# Show number of hosts, listings, regions, neighbourhoods and room types
 airbnb_singapore %>% summarize(n_hosts = n_distinct(host_id), n_listings = n_distinct(id), n_regions = n_distinct(neighbourhood_group), n_neighbourhoods = n_distinct(neighbourhood), n_types = n_distinct(
 room_type))
 
@@ -70,3 +94,198 @@ scale_x_log10() + ggtitle("Prices of vs. number of listings") + xlab("Prices") +
 #Check distributions of ratings in the training set
 airbnb_singapore %>% ggplot(aes(price)) + geom_histogram(bins = 30, binwidth=0.2, color = "azure3", fill = "deepskyblue4") +
 ggtitle("Pricing distribution")
+
+#Check the most common prices
+most_common_prices <- airbnb_singapore %>% group_by(price) %>% summarize(count = n()) %>% top_n(20, count) %>% arrange(desc(count))
+most_common_prices
+
+#Check distributions of availability in the training set
+airbnb_singapore %>% ggplot(aes(availability_365)) + geom_histogram(bins = 20, fill = "#009999") +
+ggtitle("Availability")
+
+#Check distributions of listings in the various regions in training set
+airbnb_singapore %>% ggplot(aes(neighbourhood_group)) + geom_bar(bins = 20, fill = "#3399FF") +
+ggtitle("Number of listings in regions")
+
+#Check distributions of listings in the various regions in training set
+avg_prices <- airbnb_singapore %>% group_by(neighbourhood_group) %>%
+summarize(region_avg = mean(price)) %>%
+arrange(-region_avg)
+avg_prices
+#Display them in a histogram
+avg_prices %>% ggplot(aes(reorder(neighbourhood_group, region_avg), region_avg, fill = region_avg)) +
+geom_bar(stat = "identity") + coord_flip() +
+scale_fill_distiller(palette = "YlOrRed") + labs(y = "Region price mean", x = "Region") +
+ggtitle("Average Airbnb prices of regions")
+
+#First, save the number of listings in each neighbourhood.
+
+top_neighbourhoods <- airbnb_singapore %>% group_by(neighbourhood) %>% summarize(count = n()) %>% top_n(20, count) %>% arrange(desc(count))
+top_neighbourhoods
+
+#We can also show them in a bar chart.
+top_neighbourhoods %>% ggplot(aes(reorder(neighbourhood, count), count, fill = count)) +
+geom_bar(stat = "identity") + coord_flip() +
+scale_fill_distiller(palette = "Reds") + labs(y = "Number", x = "Neighbourhood") +
+ggtitle("Popular neighbourhoods")
+
+#Check distributions of listings in the various room types in training set
+airbnb_singapore %>% ggplot(aes(room_type)) + geom_bar(bins = 20, fill = "#9999FF") +
+ggtitle("Number of listings in regions")
+
+#Check distributions of review count in the training set
+airbnb_singapore %>% ggplot(aes(number_of_reviews)) + geom_histogram(bins = 30, fill = "#009999") +
+ggtitle("Reviews")
+
+#Now, we import the text file with the Airbnb names. We can read the file from GitHub
+filePath <- "https://github.com/sarahtomori/cyo-airbnb-singapore/blob/master/singapore-airbnb-names.txt"
+text <- readLines(filePath)
+
+#Read the data as a corpus
+docs <- Corpus(VectorSource(text))
+
+#Inspect the element of the document
+inspect(docs)
+
+#Replace special characters with text
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+docs <- tm_map(docs, toSpace, "!")
+
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove your own stop word
+# specify your stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+# docs <- tm_map(docs, stemDocument)
+
+#First, create a matrix that contains the frequency of the words. 
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m), decreasing=TRUE)
+d <- data.frame(word = names(v), freq=v)
+head(d, 10)
+
+#Create a wordcloud that visualises the most common words used to describe the Airbnbs. 
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+max.words=200, random.order=FALSE, rot.per=0.35, 
+colors=brewer.pal(8, "Dark2"))
+
+#Show the 10 most frequently used words.
+head(d, 10)
+
+#and plot them
+barplot(d[1:10,]$freq, las = 2, names.arg = d[1:10,]$word,
+col ="#FF9999", main ="Most frequent words",
+ylab = "Word frequencies")
+
+#Check whether the variables are correlated.
+
+cordata <- airbnb_singapore[, c(1,3,7,8,10,11,12,14,15,16)]
+head(cordata)
+
+cordata.cor = cor(cordata, method = c("spearman"))
+cordata.rcorr = rcorr(as.matrix(cordata))
+cordata.rcorr
+
+#Next, the following code can be used to extract the p-values from the data, using the following code:
+cordata.coeff = cordata.rcorr$r
+cordata.coeff
+
+cordata.p = cordata.rcorr$P
+cordata.p
+
+#The correlation plot can be visualised using the corrplot() function
+corrplot(cordata.cor)
+
+#Data analysis
+#First, we define the function that calculates RMSE
+RMSE <- function(true_price, predicted_prices){
+sqrt(mean((true_price - predicted_price)^2, na.rm=T))
+}
+
+#Calculate average price of Airbnb prices in Singapore
+avg_price <- mean(airbnb_singapore$price)
+avg_price
+
+#Check squared loss
+mean((avg_price - validation$price)^2)
+
+#Create lm model
+fit <- lm(price ~ host_id + longitude + latitude + latitude + number_of_reviews + calculated_host_listings_count + neighbourhood_group + neighbourhood + room_type + calculated_host_listings_count + last_review + availability_365, data = airbnb_singapore)
+step(fit, direction = "backward", trace = FALSE) #use backward elimination to remove redundant variables
+
+#Let's look at the coefficients
+fit$coefficients
+
+#Save neighbourhood and room_type variables as factor in both training and validation sets
+validation$neighbourhood <- as.factor(validation$neighbourhood)
+airbnb_singapore$neighbourhood <- as.factor(airbnb_singapore$neighbourhood)
+validation$room_type <- as.factor(validation$room_type)
+airbnb_singapore$room_type <- as.factor(airbnb_singapore$room_type)
+
+#add all levels of 'neighbourhood' in 'validation' dataset to fit$xlevels[["neighbourhood"]] in the fit object
+fit$xlevels[["neighbourhood"]] <- union(fit$xlevels[["neighbourhood"]], levels(validation[["neighbourhood"]]))
+
+#add all levels of 'room_type' in 'validation' dataset to fit$xlevels[["room_type"]] in the fit object
+fit$xlevels[["room_type"]] <- union(fit$xlevels[["room_type"]], levels(validation[["room_type"]]))
+
+#Use the predict function
+y_hat <- predict(fit, validation)
+
+#Check squared loss
+mean((y_hat - validation$price)^2)
+RMSE(validation$price, y_hat)
+
+#Find the earliest review date in the data set
+sort.POSIXlt(airbnb_singapore$last_review, origin = "1970-01-01", tz = "GMT")
+
+#Replace missing values in the last_review variable with the date of the first review
+airbnb_singapore$last_review <- ifelse(is.na(airbnb_singapore$last_review), as.Date(2013-10-21,  origin = "2013-10-21", tz = "GMT"), airbnb_singapore$last_review)
+
+airbnb_singapore[is.na(airbnb_singapore)] <- 0
+
+
+#Define the dependent variable
+y_var <- airbnb_singapore$price
+
+#Define the independent variables
+x_var <- data.matrix(airbnb_singapore[, c("host_id", "longitude", "latitude", "calculated_host_listings_count", "neighbourhood", "last_review", "room_type", "availability_365")])
+
+# Setting the range of lambda values
+lambda_seq <- 10^seq(2, -2, by = -.1)
+
+train <- data.frame(x_var, y_var)
+
+model_glmnet <- train(y_var ~ ., data = train,
+                      method = "glmnet",
+                      metric = "RMSE",
+                      na.action = na.replace,
+                      alpha = 0, lambda  = lambda_seq
+)
+
+#Check the model
+summary(model_glmnet)
+model_glmnet
+
+cvfit = cv.glmnet(x_var, y_var)
+
+#Choose the optimal lambda value
+# Using cross validation glmnet
+ridge_cv <- cv.glmnet(x_var, y_var, alpha = 0, lambda = lambdas)
+# Best lambda value
+best_lambda <- model_glmnet$lambda.min
+best_lambda
+
+
